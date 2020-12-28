57df9c60809c515fbeecfd32e7dcbb20a6aaefda
Geutodun <67596860+Geutodun@users.noreply.github.com>
2020-12-28 17:56:32 +0900

    Update practice21.py

--- practice21.py
+++ practice21.py
@@ -1,4 +1,4 @@
-def people(R, N, A):
+def people(R, N, A):    #기본 값
     print("이름:{0}\t나이:{1}\t주 언어:{2}".format(R, N, A))
 
 
@@ -14,3 +14,9 @@ def person(E, B=18, D="자바"):
 
 person("이")
 person("박")
+
+def H(F, G, K):   #키워드 값
+    print(F, G, K)
+
+H(F="정", G=4, K="언어")
+H(G=3, K="C", F="강")
