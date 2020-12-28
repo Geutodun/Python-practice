2a71a6c9c0b6fa4010539a5eb715e04f8d192e66
Geutodun <67596860+Geutodun@users.noreply.github.com>
2020-12-28 13:39:06 +0900

    Create practice21.py

---
+++ practice21.py
@@ -0,0 +1,16 @@
+def people(R, N, A):
+    print("이름:{0}\t나이:{1}\t주 언어:{2}".format(R, N, A))
+
+
+people("정", 19, "파이썬")
+
+
+
+
+def person(E, B=18, D="자바"):
+    print("이름:{0}\t나이:{1}\t주 언어:{2}".format(E, B, D))
+
+
+
+person("이")
+person("박")
